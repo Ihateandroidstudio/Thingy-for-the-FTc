ef550bfb092a9479b712bdac616f806b1e6cda04
luffymanno831-ops <luffymanno831@gmail.com>
2025-09-24 16:17:58 -0400

    Create HelloWorld.java
    Added in my first tutorial class.

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@Disabled
+@Autonomous
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Coach Pratt");
+    }
+
+    @Override
+    public void loop() {
+    }
+
+    // Single Line Comment
+    /*
+    1. Hello: World, change the telementary date to display "Hello: Your name"
+    2. Run this code in the Autonomous section of your DS
+     */
+}
