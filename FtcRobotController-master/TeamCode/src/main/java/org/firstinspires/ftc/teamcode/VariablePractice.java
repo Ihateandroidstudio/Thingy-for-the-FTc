b6c8dd41e17dd4d777af6b54eb50a2bd17dfeaa8
luffymanno831-ops <luffymanno831@gmail.com>
2025-09-25 15:57:21 -0400

    yftf

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablePractice.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VariablePractice extends OpMode {
+    @Override
+    public void init() {
+        int teamNumber = 23014;
+        double motorSpeed = 0.75;
+        boolean clawClosed = true;
+        String teamName = "The Flying Dutchman";
+        int motorAngle = 90;
+
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("motor speed", motorSpeed);
+        telemetry.addData("claw closed", clawClosed);
+        telemetry.addData("Name", teamName);
+        telemetry.addData("motor angle", motorAngle);
+    }
+
+    @Override
+    public void loop() {
+        /*
+        1. change the String  variable "name" to you team name.
+        2. create an int called "motorAngle" and store an angle between 0-180. display in this in your init
+        method.
+         */
+    }
+}
