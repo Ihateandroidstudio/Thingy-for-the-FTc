5ecf4374e0ca07a910a1e817dcc8fdba195e5c48
shawn <shawnyang1010@gmail.com>
2025-10-01 15:56:07 -0400

    removed disable

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@Disabled
 @TeleOp
 public class GamePadPractice extends OpMode  {
 
