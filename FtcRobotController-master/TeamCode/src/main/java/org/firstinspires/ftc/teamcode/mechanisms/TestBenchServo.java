e9b682914c2864f495ede031fc1b41b99a387487
shawn <shawnyang1010@gmail.com>
2025-10-09 15:54:48 -0400

    SERVO STUFF

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBenchServo.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class TestBenchServo {
+    private Servo servoPos;
+    private CRServo servoRot;
+
+
+    public void init(HardwareMap hwMap){
+        servoPos = hwMap.get(Servo.class,"posservo");
+        servoRot = hwMap.get(CRServo.class,"contservo");
+        servoPos.scaleRange(0.5,1.0); //set range from midpoint to 180 degrees
+        servoPos.setDirection(Servo.Direction.REVERSE);
+        servoRot.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void setServoPos(double angle) {
+        servoPos.setPosition(angle);
+    }
+
+    public void setServoRot(double power) {
+        servoRot.setPower(power);
+    }
+}
