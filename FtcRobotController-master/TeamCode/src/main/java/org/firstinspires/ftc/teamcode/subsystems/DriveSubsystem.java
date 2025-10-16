1c51cad0ad2c87be82ae7ddc1b4190f2cedd3176
shawn <shawnyang1010@gmail.com>
2025-10-16 15:49:09 -0400

    redo

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class DriveSubsystem extends{
+import com.arcrobotics.ftclib.command.Subsystem;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class DriveSubsystem extends SubsystemBase {
+    private Motor frontleft;
+    private Motor frontright;
+    private Motor backright;
+    private Motor backleft;
+
+    private String frontleftmotor = "frontleft";
+    private String frontrightmotor = "frontleft";
+    private String backrightmotor = "backright";
+    private String backleftmotor = "backleft";
+
+
+    public DriveSubsystem (HardwareMap hardwareMap){
+        frontleft = new Motor(hardwareMap, frontleftmotor, Motor.GoBILDA.RPM_312);
+        frontright = new Motor(hardwareMap, frontrightmotor, Motor.GoBILDA.RPM_312);
+        backright = new Motor(hardwareMap, backrightmotor, Motor.GoBILDA.RPM_312);
+        backleft = new Motor(hardwareMap, backleftmotor, Motor.GoBILDA.RPM_312);
+
+
+
+
+
+    }
 }
