4d7be417644707087afa04ee4b1dd0a92c8f3d71
shawn <shawnyang1010@gmail.com>
2025-10-16 16:40:04 -0400

    teleop

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.button.Trigger;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+
+@TeleOp(name = "MainTeleop")
+public class Teleop extends CommandOpMode {
+    private DriveSubsystem drive_subsystem;
+    private GamepadEx drive_controller;
+
+    private Trigger left_trigger,right_trigger;
+
+    @Override
+    public void initialize(){
+
+        drive_controller = new GamepadEx(gamepad1);
+
+        drive_subsystem = new DriveSubsystem(hardwareMap);
+    }
+
+
+}
