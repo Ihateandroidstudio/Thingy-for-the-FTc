628405fc4b5c20aa5d58d5d1c791baeec20b67e8
luffymanno831-ops <luffymanno831@gmail.com>
2025-11-19 15:41:27 -0500

    fixed spelling errors

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TwoWheelOutake.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TwoWheelOutake.java
@@ -7,21 +7,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "Two Wheel Outake")
 public class TwoWheelOutake extends LinearOpMode {
 
-    private DcMotor outtakeLeft;
-    private DcMotor outtakeRight;
+    private DcMotor outakeLeft;
+    private DcMotor outakeRight;
 
-    // able to adjust the power
+    // able to adjust the power just change the numbers
     private final double OUTTAKE_POWER = 0.8;
     private final double REVERSE_POWER = -0.8;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        outtakeLeft = hardwareMap.get(DcMotor.class, "outtakeLeft");
-        outtakeRight = hardwareMap.get(DcMotor.class, "outtakeRight");
+        outakeLeft = hardwareMap.get(DcMotor.class, "outtakeLeft");
+        outakeRight = hardwareMap.get(DcMotor.class, "outtakeRight");
 
-        // most times one wheel is mirrored, so reverse one motor
-        outtakeRight.setDirection(DcMotor.Direction.REVERSE);
+        // most of the time one wheel is mirrored, so reverse one motor
+        outakeRight.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addLine("Ready");
         telemetry.update();
@@ -29,26 +29,26 @@ public class TwoWheelOutake extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            // Right trigger →  OUTTAKE
+            // Right trigger →  does OUTTAKE
             if (gamepad1.right_trigger > 0.1) {
-                outtakeLeft.setPower(OUTTAKE_POWER);
-                outtakeRight.setPower(OUTTAKE_POWER);
+                outakeLeft.setPower(OUTTAKE_POWER);
+                outakeRight.setPower(OUTTAKE_POWER);
             }
 
-            // Left trigger → reverse (if needed)
+            // Left trigger → makes it reverse (if needed)
             else if (gamepad1.left_trigger > 0.1) {
-                outtakeLeft.setPower(REVERSE_POWER);
-                outtakeRight.setPower(REVERSE_POWER);
+                outakeLeft.setPower(REVERSE_POWER);
+                outakeRight.setPower(REVERSE_POWER);
             }
 
-            // Otherwise let go to stop the outake
+            // Otherwise let go to stop
             else {
-                outtakeLeft.setPower(0);
-                outtakeRight.setPower(0);
+                outakeLeft.setPower(0);
+                outakeRight.setPower(0);
             }
 
-            telemetry.addData("Left Power", outtakeLeft.getPower());
-            telemetry.addData("Right Power", outtakeRight.getPower());
+            telemetry.addData("Left Power", outakeLeft.getPower());
+            telemetry.addData("Right Power", outakeRight.getPower());
             telemetry.update();
         }
     }
